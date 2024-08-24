8ac77220559ce3f4050372f9bbbbac7ae33acdfc
ipipman <ipipman@163.com>
2024-08-24 19:28:49 +0800

    print started log

--- spring-cloud-sample-demo/springcloud-provider/src/main/java/cn/ipman/sc/SpringcloudProviderApplication.java
+++ spring-cloud-sample-demo/springcloud-provider/src/main/java/cn/ipman/sc/SpringcloudProviderApplication.java
@@ -1,8 +1,10 @@
 package cn.ipman.sc;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
@@ -12,4 +14,14 @@ public class SpringcloudProviderApplication {
         SpringApplication.run(SpringcloudProviderApplication.class, args);
     }
 
+
+    @Value("${a.b}")
+    private String ab;
+
+    @Bean
+    ApplicationRunner applicationRunner() {
+        return args -> {
+            System.out.println("springcloud-provider started: " + ab);
+        };
+    }
 }
