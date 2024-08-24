1cd765a5dbc034c642e1915d0ed0663ac305f82e
ipipman <ipipman@163.com>
2024-08-24 19:03:35 +0800

    refactor user entity

--- spring-cloud-sample-demo/springcloud-api/src/main/java/cn/ipman/sc/api/model/User.java
+++ spring-cloud-sample-demo/springcloud-api/src/main/java/cn/ipman/sc/api/model/User.java
@@ -1,7 +1,9 @@
 package cn.ipman.sc.api.model;
 
+import cn.ipman.sc.common.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 /**
@@ -13,9 +15,18 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+@EqualsAndHashCode(callSuper = true)
+public class User extends BaseEntity {
 
-    private long id;
     private String name;
     private int age;
+
+    public User(long id, String name, int age) {
+        setId(id);      // 修改父类id
+        this.name = name;
+        this.age = age;
+        long now = System.currentTimeMillis();
+        setCreateTime(now); // 父类创建时间
+        setUpdateTime(now); // 父类修改时间
+    }
 }
